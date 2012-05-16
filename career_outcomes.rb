7e9f8070b27f0b558825968d353393ac871ffd6d
Stuart Wagner <stuwags@gmail.com>
2012-05-16 16:06:37 -0400

    initial commit

---
+++ career_outcomes.rb
@@ -0,0 +1,53 @@
+
+# $stdout.flush
+# puts "This program will predict your career outcomes from taking James Cropcho's introduction to Ruby Course"
+# sleep(2)
+# puts "James is a fun teacher who taught me how to make this fun program."
+# sleep(2)
+# puts "Lets see what happens in your life based on the choices you make."
+# sleep(2)
+# puts "Will you make stupid decisions, or will you make decisions to rule the world?"
+# sleep(2)
+# puts "We will find out!"
+
+
+# #This method describes what happens when you eat too much candy in class
+# def candy_impact(candy_per_class)    #How do I add extra lines with \n
+# 	if 
+# 		candy_per_class > 2
+# 		puts "	You die from too much sugar within a year."
+# 		sleep(1.5)
+# 		puts "	You never become a guru, priate, ninja, rockstar, senseis or any other stupid Web 2.0 buzzword."
+# 		sleep(1.5)
+# 		puts "	You die a non-rubyist!"
+# 	else
+# 		puts "	You still might have a nice life in front of you."
+# 	end
+# end
+
+# #This is calling what happens based on how much candy people eat, and shows what happens
+# 	puts "This is what happens when you eat only two candies per day."
+# 	puts ""
+# 	candy_impact(2)
+# 	sleep(3)
+# 	puts ""
+# 	puts "Now this is what happens when you eat three or more candies per day."
+# 	puts ""
+# 	sleep(3)
+# 	candy_impact(3)
+
+#This method describes what happens when you don't do homework
+def do_homework(homework_response)
+	if boolean_response == "Yes"
+		boolean_response == true #This means that if user says yes, the response becomes true
+		puts "James doesn't blacklist you on the world wide web and ruin your career and startup hopes"
+	elsif boolean_response != "Yes"
+		boolean_response == false
+		puts "James blacklists your name on worldwide web, and reports your behavior to Allison Baum."
+	elsif boolean_response == "What homework"
+		puts "James goes into rage and throws computer off 11th floor of the building (on Tuesdays) or off"
+
+do_homework(Yes)
+
+do_homework(What homework)
+
