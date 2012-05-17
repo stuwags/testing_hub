41a1109d759adf9048a51cc837b9628c3bbb255c
Stuart Wagner <stuwags@gmail.com>
2012-05-17 12:40:41 -0400

    added loop, but isn't working yet

---
+++ work_with_arrays.rb
@@ -0,0 +1,17 @@
+songs = [
+  "The Magnetic Fields - 69 Love Songs - Parades Go By",
+  "The Magnetic Fields - Get Lost - Smoke and Mirrors",
+  "The Magnetic Fields - Get Lost - You, Me, and the Moon",
+  "The Magnetic Fields - 69 Love Songs - The Book of Love",
+  "Neutral Milk Hotel - In An Aeroplane Over the Sea - Holland 1945",
+  "Neutral Milk Hotel - In An Aeroplane Over the Sea - The King of Carrot Flowers"
+]
+
+
+songs.split(/ - /)
+
+puts songs
+
+puts songs[0]
+
+puts songs.length(%r{\s*})
