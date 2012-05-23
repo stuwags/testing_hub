bc03db221b216a23fdd2afa554146f555cd5d10d
Stuart Wagner <stuwags@gmail.com>
2012-05-23 00:21:36 -0400

    Updated homework after class. Still having trouble adding planes to hangar and not sure why.

--- work_with_arrays.rb
+++ work_with_arrays.rb
@@ -1,3 +1,11 @@
+# Given this List of Songs, Construct Arrays by Artist and Album
+# Hint: Make use of the split() String Method
+# http://www.ruby-doc.org/core-1.9.3/String.html#method-i-split
+
+
+
+
+
 songs = [
   "The Magnetic Fields - 69 Love Songs - Parades Go By",
   "The Magnetic Fields - Get Lost - Smoke and Mirrors",
@@ -8,10 +16,33 @@ songs = [
 ]
 
 
-songs.split(/ - /)
+songs_string = songs.to_s
+songs_string_cleaned_of_commas = songs_string.gsub(",", " -")
+songs_string_clean = songs_string_cleaned_of_commas.gsub('"', '').chomp
 
-puts songs
+puts songs_string_clean
+puts songs_string_clean.class
+songs_string_clean_array = songs_string_clean.split("-")
+puts "bujafuja"
+puts songs_string_clean_array
+puts songs_string_clean_array.length
+puts songs_string_clean_array.class
+# songs_string_split = songs_string.split(" - ")
+# puts songs_string_split
+# puts songs_string_split.length
+# puts songs_string_split.class
+# puts songs_string_split[0]
 
-puts songs[0]
 
-puts songs.length(%r{\s*})
+# # songs_split = songs_string.each do |string| 
+# # string.split(/ - /)
+# # end
+
+# # puts songs_split
+# # puts "break"
+# # puts songs #This means they are the same, which means my song split method didn't work
+
+# # songs_double_split = songs_split[0].split(/ - /)
+
+
+
