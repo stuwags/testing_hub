8829b392d43deb3a49b39251677e8228e42de029
Stuart Wagner <stuwags@gmail.com>
2012-05-27 14:21:32 -0400

    changes made recently

--- career_outcomes.rb
+++ career_outcomes.rb
@@ -1,29 +1,19 @@
+require "./career_outcomes_helpers.rb"
 
-$stdout.flush
-puts "This program will predict your career outcomes from taking James Cropcho's introduction to Ruby Course"
-sleep(2)
-puts "James is a fun teacher who taught me how to make this fun program."
-sleep(2)
-puts "Lets see what happens in your life based on the choices you make."
-sleep(2)
-puts "Will you make stupid decisions, or will you make decisions to rule the world?"
-sleep(2)
-puts "We will find out!"
+# $stdout.flush
+
+# puts "This program will predict your career outcomes from taking James Cropcho's introduction to Ruby Course"
+# sleep(2)
+# puts "James is a fun teacher who taught me how to make this fun program."
+# sleep(2)
+# puts "Lets see what happens in your life based on the choices you make."
+# sleep(2)
+# puts "Will you make stupid decisions, or will you make decisions to rule the world?"
+# sleep(2)
+# puts "We will find out!"
 
 
-#This method describes what happens when you eat too much candy in class
-def candy_impact(candy_per_class)    #How do I add extra lines with \n
-	if 
-		candy_per_class > 2
-		puts "	You die from too much sugar within a year."
-		sleep(1.5)
-		puts "	You never become a guru, priate, ninja, rockstar, senseis or any other stupid, meaningless Web 2.0 buzzword."
-		sleep(1.5)
-		puts "	You die a non-rubyist!"
-	else
-		puts "	You still might have a nice life in front of you."
-	end
-end
+
 
 #This is calling what happens based on how much candy people eat, and shows what happens
 	puts "This is what happens when you eat only two candies per day."
@@ -35,27 +25,14 @@ end
 	puts ""
 	sleep(3)
 	candy_impact(3)
+	sleep(2)
+	puts "How many candies do you eat per class?"
+	number_of_candies_string = gets.chomp
+	number_of_candies = number_of_candies_string.to_i
+	candy_impact(number_of_candies)
 
 
 
-
-
-def do_homework_case(hwresponse) #how do I make yes and else become boolean? What does Ctrl B show me?
-	case hwresponse
-	when "Yes"
-			puts "James gives you four pieces of candy and you eat them."
-		sleep (3)
-			puts ""
-			puts "Oh no, DON'T EAT THE CANDY!!!!"
-		sleep (1)
-			puts ""
-			puts candy_impact(4)
-	when "What homework?"
-		puts "James goes into rage and throws computer off 11th floor of the building (on Tuesdays) or off the floor of the other building on Thursday."
-	else
-		puts "James blacklists your name on worldwide web, and reports your behavior to Allison Baum."
-	end
-end
 
 
 puts "This is what happens if you do your homework!"
@@ -67,28 +44,28 @@ do_homework_case("What homework?")
 puts "This is what happens if you answer with some other stupid answer, other than yes and What homework."
 do_homework_case("I don't know")
 
+puts "Do you regularly do your homework?"
+user_answer_to_homework = gets.chomp
+do_homework_case(user_answer_to_homework)
+puts "Now this consequence could possibly be mitigated if Stuart makes James a sandwich."
+sleep (2)
 
-puts "This is what happens if you tell me to 'Go fuck yourself'""
+puts "Will Stuart make James a sandwich? Enter Yes, No or try acting like an asshole and entering some other stupid value."
+
+puts "This is what happens if you tell me to 'Go fuck yourself'"
 
 
-#can't figure out how to make this work?
-def angry_computer(evil_user) 
-	case evil_user
-		when "Go fuck yourself"
-			sleep (3)
-			puts "You will be very sorry for that!"
-			sleep (3)
-			puts "Fuck you. I just froze your cmd application."
-			sleep (10)
-			while evil_user == "Go fuck yourself"
-			puts "Fuck you."
-			until evil_user != "Go fuck yourself"
-			end
-		end
-	end
-end
+# #can't figure out how to make this work?
+# def angry_computer(evil_user) 
+# 	if evil_user == "Go fuck yourself"
+# 			sleep (3)
+# 			puts "You will be very sorry for that!"
+# 			sleep (3)
+# 			puts "Fuck you. I just froze your cmd application. " * 99
+# 	end
+# end
 
-angry_computer("Go fuck yourself")
+# angry_computer("Go fuck yourself")
 
 
 
